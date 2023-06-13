030000370ed7ff7f9a82e7f2c39bf9cca56598de
EladPress <presselad2@gmail.com>
2023-06-13 17:52:07 +0300

    cleaned up a bit

--- client/js/login.js
+++ client/js/login.js
@@ -1,10 +1,15 @@
 import {select} from './server_access.js'
 let source = "http://127.0.0.1:8081"
+let button = document.getElementById("submit").onclick = () => {
+    //console.log('works')
+    select('eladpress', 'elad2002')
+    .then((res) => {console.log(res)})
+}
 function checkForm(){
     return select('eladpress', 'elad2002')
    
 }
 // checkForm()
-if (select!=err){
-    window.location.href = "../HTML/index.html"
-}
+// if (select!=err){
+//     window.location.href = "../HTML/index.html"
+// }
